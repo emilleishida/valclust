976cada30006fb002b835b06c6e8648c6916dcb7
mirjalil <vmirjalily@gmail.com>
2015-01-01 20:46:09 -0500

    valclust intra-cluster updated

--- tests/test_pdist_sampler.py
+++ tests/test_pdist_sampler.py
@@ -1,10 +1,24 @@
 import numpy as np
+import sys
 from valclust.clusterSeparation import PairwiseDistanceSampler as pds
 
 
 def test_intra_sampling():
-    d = np.random.multivariate_normal(mean=[0,0], cov=[[1, 0],[0, 1]], size=10)
-    y = np.ones(shape=(10,), dtype='int')
+
+    np.random.seed(1234)
+
+    d1 = np.random.multivariate_normal(mean=[-1,1], cov=[[1, 0],[0, 1]], size=3)
+    d2 = np.random.multivariate_normal(mean=[2,-2], cov=[[1, 0],[0, 1]], size=4)
+
+    d = np.vstack((d1, d2))
+    y = np.array([1,1,1, 2,2,2,2])
     sam_obj = pds()
 
-    sam_obj.intra_sampler(d, y, 1, size=None)
+    ds1 = sam_obj.intra_sampler(d, y, 1, size=None)
+    assert(ds1.shape[0]) == 3
+
+    ds2 = sam_obj.intra_sampler(d, y, 2, size=None)
+    assert(ds2.shape[0]) == 6
+
+if __name__ == '__main__':
+    unittest.main()
--- valclust/clusterSeparation/pdist_sampling.py
+++ valclust/clusterSeparation/pdist_sampling.py
@@ -5,6 +5,16 @@ import numpy as np
 import pyprind
 import sys
 from ..cluster import Cluster
+import editdist
+
+def cal_distance(x1, x2, method='euclidean'):
+    if method == 'euclidean':
+        diff = x1 - x2
+        sq_diff = diff * diff
+        return(np.sqrt(np.sum(sq_diff)))
+    elif method == 'editdist':
+	return(editdist.distance(x1, x2))
+
 
 class PairwiseDistanceSampler(Cluster):
     """
@@ -16,7 +26,7 @@ class PairwiseDistanceSampler(Cluster):
 
     """
 
-    def intra_sampler(self, X, y, cinx, size=None):
+    def intra_sampler(self, X, y, cinx, size=None, return_ind=False):
 	"""
 	Sampling intra-cluster pairwise distances.
 
@@ -27,9 +37,25 @@ class PairwiseDistanceSampler(Cluster):
 		size (amount of sampling), if None, all exclusive pairwise distances are computed.
 
 	"""
-	print ("Calling sample function!")
 
 	X_clust = X[self.get_members(y, cinx),:]
 	nsize,ndim = X_clust.shape
-	print("Cluster Size: %d   Numb. of Dimensions: %d" %(nsize, ndim))
+	totpw = nsize*(nsize-1)/2
+	sys.stderr.write("##\tCluster %s  --> Size: %d   NumDim.: %d  Total pairs: %d\n" %(cinx, nsize, ndim, nsize*(nsize-1)/2))
+
+	dary = np.zeros(shape=totpw, dtype='float')
+
+	if size is None:
+	    sys.stderr.write("##\tComputing all intra-cluster pairwise distances ...\n")
+
+	    n = 0
+	    for i in range(nsize):
+		for j in range(i+1, nsize):
+		    dary[n] = cal_distance(X_clust[i,:], X_clust[j,:], method='euclidean')
+		    n += 1
+	else:
+	    sys.stderr.write("##\tSampling %d intra-cluster pairwise distances ...\n" %size)
+
+
+	return(dary)
 
