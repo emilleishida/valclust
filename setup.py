f3cdc49d5894c6a781ec2fb20cd8ce049256bdf2
mirjalil <vmirjalily@gmail.com>
2015-01-15 01:12:21 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name = "valclust",
     author_email = "vmirjalily@gmail.com",
     url = "https://github.com/mirjalil/valclust",
 
-    packages = ['valclust', 'valclust.clusterSeparation',
+    packages = ['valclust', 'valclust.ExternalValidity',
 	],
 
     #package *needs* these files.
