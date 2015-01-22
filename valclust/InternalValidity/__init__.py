cca746f96f65db936aa7ef1096947158ac95ebb2
mirjalil <vmirjalily@gmail.com>
2015-01-22 11:11:15 -0500

    init InternalValidity: purity

---
+++ valclust/InternalValidity/__init__.py
@@ -0,0 +1,8 @@
+# Vahid Mirjalili 12/31/2014
+# Clustering Validation and Analysis
+# Submodules for sampling intra and inter cluster distances
+
+
+from ..cluster import Cluster
+
+
---
+++ valclust/InternalValidity/comp_clusterings.py
@@ -0,0 +1,19 @@
+# Vahid Mirjalili
+# Clustering Validation and Analysis
+
+import numpy as np
+import pyprind
+import sys
+from ..cluster import Cluster
+import editdist
+
+def clusterPurity(Cluster, cinx, g):
+    """ Compute the purity of an individual cluster cinx 
+	with respect to a given clustering solution 
+	g (which could be ground-truth).
+    """
+    pass
+
+
+def totalPurity(Cluster, cinx, g):
+   pass
